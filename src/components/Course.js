578e9447bfe6d0fecb623e5ce9f32244023c4cf6
dpak1999 <deepakkumardash313@gmail.com>
2020-10-27 22:16:04 +0530

    updating courses and deleting progress

--- src/components/Course.js
+++ src/components/Course.js
@@ -1,41 +1,56 @@
-import React from 'react';
+import React from "react";
 
 export default function Course({ course, refreshCourses }) {
-    const markCoursePurchased = async () => {
-        //TODO mark course as purchased
-    };
+  const markCoursePurchased = async () => {
+    //TODO mark course as purchased
+    try {
+      await fetch("/api/courses", {
+        method: "PUT",
+        body: JSON.stringify({ ...course, purchased: true }),
+      });
+      refreshCourses();
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    const deleteCourse = async () => {
-        //TODO delete course
-    };
-    return (
-        <div className="list-group-item">
-            <a href={course.link}>
-                <h4 className="list-group-item-heading">{course.name}</h4>
-            </a>
-            <p>
-                Tags:{' '}
-                {course.tags &&
-                    course.tags.map((tag, index) => (
-                        <span className="badge badge-primary mr-2" key={index}>
-                            {tag}
-                        </span>
-                    ))}
-            </p>
-            {!course.purchased && (
-                <button
-                    className="btn btn-sm btn-primary"
-                    onClick={markCoursePurchased}
-                >
-                    Purchased
-                </button>
-            )}
-            <button
-                className="btn btn-sm btn-danger ml-2"
-                onClick={deleteCourse}
-            >
-                Delete
-            </button>
-        </div>
-    );
+  const deleteCourse = async () => {
+    //TODO delete course
+    try {
+      await fetch("/api/courses", {
+        method: "DELETE",
+        body: JSON.stringify({ id: course.id }),
+      });
+      refreshCourses();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  return (
+    <div className="list-group-item">
+      <a target="blank" href={course.link}>
+        <h4 className="list-group-item-heading">{course.name}</h4>
+      </a>
+      <p>
+        Tags:{" "}
+        {course.tags &&
+          course.tags.map((tag, index) => (
+            <span className="badge badge-primary mr-2" key={index}>
+              {tag}
+            </span>
+          ))}
+      </p>
+      {!course.purchased && (
+        <button
+          className="btn btn-sm btn-primary"
+          onClick={markCoursePurchased}
+        >
+          Completed
+        </button>
+      )}
+      <button className="btn btn-sm btn-danger ml-2" onClick={deleteCourse}>
+        Delete
+      </button>
+    </div>
+  );
 }

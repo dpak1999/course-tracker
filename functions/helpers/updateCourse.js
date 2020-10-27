2c082411a9f3c9fffd30c4553ef48b477625b15d
dpak1999 <deepakkumardash313@gmail.com>
2020-10-27 20:58:17 +0530

    update a course

--- functions/helpers/updateCourse.js
+++ functions/helpers/updateCourse.js
@@ -1,5 +1,8 @@
-const { table } = require('./airtable');
-const formattedReturn = require('./formattedReturn');
+const { table } = require("./airtable");
+const formattedReturn = require("./formattedReturn");
 module.exports = async (event) => {
-    // TODO: update course
+  // TODO: update course
+  const { id, ...fields } = JSON.parse(event.body);
+  const updatedCourse = await table.update([{ id, fields }]);
+  return formattedReturn(200, updatedCourse);
 };
